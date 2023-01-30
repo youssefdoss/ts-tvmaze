1e723e68acad276ab828151a60a1e852c6b2fc47
Youssef Doss <youssefdoss97@gmail.com>
2023-01-30 15:14:16 -0500

    through adding images

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,6 +5,24 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const BASE_API_URL = 'https://api.tvmaze.com/';
+const DEFAULT_IMAGE_URL = 'https://tinyurl.com/tv-missing';
+
+interface ShowInterface {
+  id: number,
+  name: string,
+  summary: string,
+  image: string
+}
+
+interface ShowFromApiInterface {
+  show: {
+    id: number,
+    name: string,
+    summary: string,
+    image: { medium: string } | null
+  }
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,32 +31,25 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string): Promise<ShowInterface[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const shows = await axios.get(
+    `${BASE_API_URL}search/shows`, {params: { q: term } });
+  const formattedShows = shows.data.map((singleShow: ShowFromApiInterface) => {
+    const show = singleShow.show
+    return {
+    id: show.id,
+    name: show.name,
+    summary: show.summary,
+    image: show.image?.medium || DEFAULT_IMAGE_URL
+  }});
+  return formattedShows;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: ShowInterface[]) {
   $showsList.empty();
 
   for (let show of shows) {
@@ -46,7 +57,7 @@ function populateShows(shows) {
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src="${show.image}"
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
